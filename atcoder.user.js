e0e6b060eae884a58f1c22bc075f1d54247b20ca
Summ Tokhshima <fishfishfish@hash.fyi>
2020-12-30 13:01:55 +0900

    v1.5.4 ACLのバージョンを1.2に固定した

--- atcoder.user.js
+++ atcoder.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AtCoder Easy Test
 // @namespace    http://atcoder.jp/
-// @version      1.5.3
+// @version      1.5.4
 // @description  Make testing sample cases easy
 // @author       magurofly
 // @match        https://atcoder.jp/contests/*/tasks/*
@@ -20,7 +20,7 @@
 
 (function script() {
 
-const VERSION = "1.5.3";
+const VERSION = "1.5.4";
 
 if (typeof unsafeWindow !== "undefined") {
     console.log(unsafeWindow);
@@ -280,7 +280,7 @@ const codeRunner = (function() {
                 files.add("internal_type_traits");
             }
 
-            const ACLBase = "https://cdn.jsdelivr.net/gh/atcoder/ac-library/atcoder/";
+            const ACLBase = "https://cdn.jsdelivr.net/gh/atcoder/ac-library@1.2/atcoder/";
 
             const codePromises = [];
             const codes = [];
