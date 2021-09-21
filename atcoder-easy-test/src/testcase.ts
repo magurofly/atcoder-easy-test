2613e9be74674e3f2b36ed9bad91b8ca8326b1b8
magurofly <fishfishfish@hash.fyi>
2021-09-22 02:13:50 +0900

    update

---
+++ atcoder-easy-test/src/testcase.ts
@@ -0,0 +1,37 @@
+export interface TestCase {
+  input: string,
+  output: string,
+  anchor: HTMLElement,
+}
+
+export function getTestCases(): TestCase[] {
+  const selectors = [
+    ["#task-statement p+pre.literal-block", ".section"], // utpc2011_1
+    ["#task-statement pre.source-code-for-copy", ".part"],
+    ["#task-statement .lang>*:nth-child(1) .div-btn-copy+pre", ".part"],
+    ["#task-statement .div-btn-copy+pre", ".part"],
+    ["#task-statement>.part pre.linenums", ".part"], // abc003_4
+    ["#task-statement>.part:not(.io-style)>h3+section>pre", ".part"],
+    ["#task-statement pre", ".part"],
+  ];
+  
+  for (const [selector, closestSelector] of selectors) {
+    const e = [... document.querySelectorAll(selector)].filter(e => {
+      if (e.closest(".io-style")) return false; // practice2
+      return true;
+    });
+    if (e.length == 0) continue;
+    const testcases = [];
+    for (let i = 0; i < e.length; i += 2) {
+      const container = e[i].closest(closestSelector) || e[i].parentElement;
+      testcases.push({
+        input: (e[i]||{}).textContent,
+        output: (e[i+1]||{}).textContent,
+        anchor: container.querySelector("h3"),
+      });
+    }
+    return testcases;
+  }
+  
+  return [];
+};
